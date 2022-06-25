4efcce32b8bb9dc0c78648e95d3d897512aa3f6d
monkjunior <vungocson998@gmail.com>
2022-06-25 15:55:07 +0700

    Implemented binary tree insertion sort

--- algorithms/sorting/sorting.go
+++ algorithms/sorting/sorting.go
@@ -17,3 +17,45 @@ func InsertionSort(arr []int) []int {
 	}
 	return arr
 }
+
+type binaryTree struct {
+	value       int
+	left, right *binaryTree
+}
+
+func add(tree *binaryTree, value int) *binaryTree {
+	if tree == nil {
+		tree = new(binaryTree)
+		tree.value = value
+		return tree
+	}
+
+	if value <= tree.value {
+		tree.left = add(tree.left, value)
+		return tree
+	}
+
+	tree.right = add(tree.right, value)
+	return tree
+}
+
+func appendSlice(values []int, tree *binaryTree) []int {
+	if tree == nil {
+		return values
+	}
+	values = appendSlice(values, tree.left)
+	values = append(values, tree.value)
+	values = appendSlice(values, tree.right)
+	return values
+}
+
+func InsertionSort_BinaryTree(originSlice []int) (sortedSlice []int) {
+	var binTree *binaryTree
+	for _, value := range originSlice {
+		binTree = add(binTree, value)
+	}
+
+	sortedSlice = []int{}
+	sortedSlice = appendSlice(sortedSlice, binTree)
+	return sortedSlice
+}
--- algorithms/sorting/sorting_test.go
+++ algorithms/sorting/sorting_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestInsertionSort(t *testing.T) {
+func Test_InsertionSort(t *testing.T) {
 	type args struct {
 		arr []int
 	}
@@ -29,7 +29,7 @@ func TestInsertionSort(t *testing.T) {
 	}
 }
 
-func TestRevert(t *testing.T) {
+func Test_Revert(t *testing.T) {
 	type args struct {
 		arr []int
 	}
@@ -48,6 +48,30 @@ func TestRevert(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Revert(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Revert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_InsertionSort_BinaryTree(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantSorted_arr []int
+	}{
+		{"#0 Empty array", args{[]int{}}, []int{}},
+		{"#1", args{[]int{1, 0}}, []int{0, 1}},
+		{"#2", args{[]int{1, 0, 3}}, []int{0, 1, 3}},
+		{"#3", args{[]int{1, 0, 3, 2}}, []int{0, 1, 2, 3}},
+		{"#4", args{[]int{3, 2, 1, 4, 7, 8, 6, 9, 5, 0}}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSorted_arr := InsertionSort_BinaryTree(tt.args.arr); !reflect.DeepEqual(gotSorted_arr, tt.wantSorted_arr) {
+				t.Errorf("InsertionSort_BinaryTree() = %v, want %v", gotSorted_arr, tt.wantSorted_arr)
 			}
 		})
 	}
